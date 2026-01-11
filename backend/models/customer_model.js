7f6631646867552af14213f13164b4694abe364d
Kartsoful <kartsoful@gmail.com>
2026-01-11 06:07:17 +0200

    Endpointit tehty, testaamatta

--- backend/models/customer_model.js
+++ backend/models/customer_model.js
@@ -3,6 +3,22 @@ const db = require('../database');
 const customer = {
     getAll:function(callback){
         return db.query("SELECT * FROM customer", callback);
+    },
+
+    getOne:function(idcustomer, callback){
+        return db.query("SELECT * FROM customer WHERE idcustomer=?",[idcustomer], callback);
+    },
+
+    add:function(customer, callback){
+        return db.query("INSERT INTO customer VALUES(?,?,?,?)",[customer.fname,customer.lname,customer.street_address,customer.city],callback);
+    },
+
+    update:function(customer, idcustomer, callback){
+        return db.query("UPDATE customer SET fname=?,lname=?,street_address=?,city=? WHERE idcustomer=?"[customer.fname,customer.lname,customer.street_address,customer.city,idcustomer],callback);
+    },
+
+    delete:function(customer, idcustomer, callback){
+        return db.query("DELETE FROM customer WHERE idcustomer=?"[idcustomer],callback);
     }
 }
 
